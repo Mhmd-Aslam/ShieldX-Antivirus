5602b72b89129aaaafeed1ca6545624e663bbf55
Milansuman <milansuman2016@gmail.com>
2025-01-14 15:20:49 +0530

    add disassembler for Windows executables

---
+++ static/metadata.py
@@ -0,0 +1,80 @@
+import pefile
+import peutils
+import magic
+from datetime import datetime
+from capstone import *
+from typing import Dict
+
+class StaticAnalyzer:
+    """Base class for all static analysis tasks"""
+    def __init__(self, path):
+        self.path = path
+
+    def get_strings(self):
+        with open(self.path, "rb") as file:
+            return file.read().decode("utf-8", errors="ignore")
+        
+    def file_type(path):
+        return magic.from_file(path)
+    
+    def file_type(self):
+        return magic.from_file(self.path)
+
+class PEAnalyzer(StaticAnalyzer):
+    """Analyzer class for windows executables"""
+    def __init__(self, path):
+        super().__init__(path)
+        self.executable = pefile.PE(path) #Avoid wasting time on directories
+
+    def __str__(self):
+        return self.executable.dump_info()
+    
+    def info(self):
+        return {
+            "machine_type": self.executable.FILE_HEADER.Machine,
+            "timestamp": datetime.fromtimestamp(self.executable.FILE_HEADER.TimeDateStamp).strftime("%d/%m/%Y, %H:%M:%S"),
+            "file_type": super().file_type(),
+            "packed": peutils.is_probably_packed(self.executable),
+            "size_of_code": hex(self.executable.OPTIONAL_HEADER.SizeOfCode),
+            "entry_point": hex(self.executable.OPTIONAL_HEADER.AddressOfEntryPoint),
+            "image_base": hex(self.executable.OPTIONAL_HEADER.ImageBase)
+        }
+    
+    def disassemble(self, section: pefile.SectionStructure):
+        try:
+            image_base = self.executable.OPTIONAL_HEADER.ImageBase
+                
+            data = self.executable.get_memory_mapped_image()
+            
+            start_offset = section.VirtualAddress
+            code_size = section.SizeOfRawData
+            
+            # Initialize disassembler for x64
+            md = Cs(CS_ARCH_X86, CS_MODE_64)
+            
+            # Enable additional details (optional)
+            md.detail = True
+            
+            # Disassemble the entire code section
+            code_bytes = data[start_offset:start_offset + code_size]
+            
+            for inst in md.disasm(code_bytes, start_offset+image_base):
+                if inst.bytes == b'\x00\x00':
+                    continue
+
+                yield {
+                    "address": inst.address,
+                    "mnemonic": inst.mnemonic,
+                    "bytes": inst.bytes.hex(" "),
+                    "arguments": inst.op_str
+                }
+                
+        except Exception as e:
+            print(f"Disassembly error: {str(e)}")
+            return []
+
+    
+if __name__ == "__main__":
+    pe = PEAnalyzer("tests/test_sample.exe")
+    for instruction in pe.disassemble(pe.executable.sections[2]):
+        print(f"{hex(instruction["address"])}: {instruction["mnemonic"]} {instruction["arguments"]}")
